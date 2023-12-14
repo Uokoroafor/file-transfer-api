8ccaa39210d99b4d897379fdf80a647f7b2f4ff0
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2023-12-14 22:46:46 +0000

    Updating Responses

--- abstracts/response_abstract.py
+++ abstracts/response_abstract.py
@@ -1,14 +1,41 @@
-from abc import ABC
-from pydantic import BaseModel
+from dataclasses import dataclass
+from typing import Dict, Any
 
 
-class AbstractResponse(BaseModel, ABC):
+@dataclass
+class CustomResponse:
     """Abstract class for the response model.
 
     It contains methods that will be defined elsewhere in concrete classes.
     """
-    status_code: int = 200
+
     message: str
+    status_code: int = 200
+
+    def __init__(self, **kwargs):
+        """Constructor for CustomResponse. It will set the attributes of the class based on the keyword arguments.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments.
+        """
+        data ={}
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+            if key not in ['message', 'status_code']:
+                data[key] = value
+        self.data = data
+
+    def to_dict(self) -> Dict[str, Any]:
+        """Converts the class to a dictionary.
+
+        Returns:
+            Dict: Dictionary representation of the class - to make it compatible with FastAPI, we will return
+            status_code and message, then the rest of the attributes as a dictionary called data.
+        """
+        return self.__dict__
 
 
-
+if __name__ == '__main__':
+    test_response = CustomResponse(message="Welcome to the API", status_code=200, file_id="12345678-1234-5678-1234-567812345678")
+    print(test_response)
+    print(test_response.to_dict())
--- schemas/responses.py
+++ schemas/responses.py
@@ -1,10 +1,13 @@
-from abstracts.response_abstract import AbstractResponse
+from abstracts.response_abstract import CustomResponse
+from dataclasses import dataclass
 
 
-class FileUploadResponse(AbstractResponse):
+@dataclass
+class FileUploadResponse:
     """Response model for file upload"""
     file_id: str
     content_type: str
+    message: str
 
     class Config:
         schema_extra = {
@@ -17,9 +20,11 @@ class FileUploadResponse(AbstractResponse):
         }
 
 
-class FileDownloadResponse(AbstractResponse):
+@dataclass
+class FileDownloadResponse:
     """Response model for file download"""
     file_id: str
+    message: str
 
     class Config:
         schema_extra = {
@@ -31,10 +36,12 @@ class FileDownloadResponse(AbstractResponse):
         }
 
 
-class FileReplaceResponse(AbstractResponse):
+@dataclass
+class FileReplaceResponse:
     """Response model for file replace"""
     file_id: str
     content_type: str
+    message: str
 
     class Config:
         schema_extra = {
@@ -47,9 +54,11 @@ class FileReplaceResponse(AbstractResponse):
         }
 
 
-class FileDeleteResponse(AbstractResponse):
+@dataclass
+class FileDeleteResponse:
     """Response model for file delete"""
     file_id: str
+    message: str
 
     class Config:
         schema_extra = {
@@ -61,10 +70,12 @@ class FileDeleteResponse(AbstractResponse):
         }
 
 
-class FileRenameResponse(AbstractResponse):
+@dataclass
+class FileRenameResponse:
     """Response model for file rename"""
     old_file_id: str
     new_file_id: str
+    message: str
 
     class Config:
         schema_extra = {
@@ -77,8 +88,11 @@ class FileRenameResponse(AbstractResponse):
         }
 
 
-class StandardResponse(AbstractResponse):
+@dataclass
+class StandardResponse:
     """Response model for standard responses"""
+    message: str
+
     class Config:
         schema_extra = {
             "example": {
@@ -88,8 +102,11 @@ class StandardResponse(AbstractResponse):
         }
 
 
-class ErrorResponse(AbstractResponse):
+@dataclass
+class ErrorResponse:
     """Response model for errors"""
+    message: str
+
     class Config:
         schema_extra = {
             "example": {
@@ -97,3 +114,87 @@ class ErrorResponse(AbstractResponse):
                 "status_code": 404
             }
         }
+
+
+@dataclass
+class DatabaseSelectResponse:
+    """Response model for database select"""
+    file_id: str
+    name: str
+    content_type: str
+    size: int
+    created_timestamp: str
+    last_modified_timestamp: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "file_id": "12345678-1234-5678-1234-567812345678",
+                "name": "image.jpg",
+                "content_type": "image/jpeg",
+                "size": 123456,
+                "created_timestamp": "2021-01-01T00:00:00",
+                "last_modified_timestamp": "2021-01-01T00:00:00",
+                "status_code": 200,
+                "message": "File selected"
+            }
+        }
+
+
+@dataclass
+class DatabaseUpdateResponse:
+    """Response model for database update"""
+    file_id: str
+    name: str
+    content_type: str
+    size: int
+    created_timestamp: str
+    last_modified_timestamp: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "file_id": "12345678-1234-5678-1234-567812345678",
+                "name": "image.jpg",
+                "content_type": "image/jpeg",
+                "size": 123456,
+                "created_timestamp": "2021-01-01T00:00:00",
+                "last_modified_timestamp": "2021-01-01T00:00:00",
+                "status_code": 200,
+                "message": "File updated"
+            }
+        }
+
+
+@dataclass
+class DatabaseDeleteResponse:
+    """Response model for database delete"""
+    file_id: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "file_id": "12345678-1234-5678-1234-567812345678",
+                "status_code": 200,
+                "message": "File deleted"
+            }
+        }
+
+
+# For each class save the config example as a json in the ../tests/test_fixtures folder
+if __name__ == '__main__':
+    import json
+    from pathlib import Path
+    import os
+
+    # Create the test_fixtures folder if it doesn't exist
+    test_fixtures_folder = Path(__file__).parent.parent / "tests" / "test_fixtures/responses"
+    if not test_fixtures_folder.exists():
+        os.mkdir(test_fixtures_folder)
+
+    # Save the config example as a json in the ../tests/test_fixtures folder
+    for cls in [FileUploadResponse, FileDownloadResponse, FileReplaceResponse, FileDeleteResponse, FileRenameResponse,
+                StandardResponse, ErrorResponse, DatabaseSelectResponse, DatabaseUpdateResponse,
+                DatabaseDeleteResponse]:
+        with open(test_fixtures_folder / f"{cls.__name__}.json", "w") as f:
+            json.dump(cls.Config.schema_extra["example"], f, indent=4)
