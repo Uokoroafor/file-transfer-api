a778251325e3889a05b482009adbf1db69e46dc6
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2023-12-14 22:49:29 +0000

    Updating Routers

---
+++ routers/fastapi_database_router.py
@@ -0,0 +1,126 @@
+from typing import List
+
+from fastapi import APIRouter
+from schemas.responses import FileUploadResponse, FileDownloadResponse, FileReplaceResponse, FileDeleteResponse, \
+    FileRenameResponse, StandardResponse, DatabaseSelectResponse
+from services.local_database_manager import LocalDatabaseManager
+from services.aws_database_manager import AWSDatabaseManager
+import os
+
+
+router = APIRouter()
+DATABASE_LOCATION = os.getenv("DATABASE_LOCATION", "local")
+
+if DATABASE_LOCATION == "local":
+    database_manager = LocalDatabaseManager()
+elif DATABASE_LOCATION == "aws":
+    database_manager = AWSDatabaseManager()
+else:
+    raise NotImplementedError(f"Database location {DATABASE_LOCATION} not implemented")
+
+
+@router.get("/")
+async def database_root() -> StandardResponse:
+    """This is the root endpoint of the database API. It will return a simple message
+
+    Returns:
+        A message welcoming the user to the database API
+    """
+    return StandardResponse(message="Welcome to the database section of the API")
+
+
+@router.get("/select/{file_id}")
+async def select_file(file_id: str) -> DatabaseSelectResponse:
+    """Select a file from the database.
+
+    Args:
+        file_id: Id of the file to select
+
+    Returns:
+        File metadata and status.
+
+    Raises:
+        FileDoesNotExistError: If the file does not exist
+        FileDownloadError: If the file download fails
+    """
+    file_record = database_manager.get_file_record(file_id)
+    return DatabaseSelectResponse(file_id=file_record.file_id, name=file_record.name,
+                                    content_type=file_record.content_type, size=file_record.size,
+                                    created_timestamp=file_record.created_timestamp,
+                                    last_modified_timestamp=file_record.last_modified_timestamp)
+
+
+@router.get("/select_all")
+async def select_all_files() -> List[DatabaseSelectResponse]:
+    """Select all files from the database.
+
+    Returns:
+        List of file metadata and status.
+
+    Raises:
+        FileDoesNotExistError: If the file does not exist
+        FileDownloadError: If the file download fails
+    """
+    file_records = database_manager.get_all_file_records()
+    file_download_responses = []
+    for file_record in file_records:
+        file_download_responses.append(DatabaseSelectResponse(file_id=file_record.file_id, name=file_record.name,
+                                                            content_type=file_record.content_type,
+                                                            size=file_record.size,
+                                                            created_timestamp=file_record.created_timestamp,
+                                                            last_modified_timestamp=file_record.last_modified_timestamp))
+
+    return file_download_responses
+
+
+@router.get("/count")
+async def count_files() -> int:
+    """Count the number of files in the database.
+
+    Returns:
+        Number of files in the database.
+
+    Raises:
+        FileDoesNotExistError: If the file does not exist
+        FileDownloadError: If the file download fails
+    """
+    return database_manager.get_count()
+
+
+@router.delete("/delete/{file_id}")
+async def delete_file(file_id: str) -> FileDeleteResponse:
+    """Delete a file from the database.
+
+    Args:
+        file_id: Id of the file to delete
+
+    Returns:
+        File metadata and status.
+
+    Raises:
+        FileDoesNotExistError: If the file does not exist
+        FileDownloadError: If the file download fails
+    """
+    file_record = database_manager.delete_file_record(file_id)
+    return FileDeleteResponse(file_id=file_record.file_id)
+
+
+# @router.put("/rename/{file_id}")
+# async def rename_file(file_id: str, new_file_id: str) -> FileRenameResponse:
+#     """Rename a file in the database.
+#
+#     Args:
+#         file_id: Id of the file to rename
+#         new_file_id: New id of the file
+#
+#     Returns:
+#         File metadata and status.
+#
+#     Raises:
+#         FileDoesNotExistError: If the file does not exist
+#         FileRenameError: If the file rename fails
+#     """
+#     file_record = database_manager.rename_file_record(file_id, new_file_id)
+#     return FileRenameResponse(old_file_id=file_id, new_file_id=file_record.file_id)
+
+
--- routers/fastapi_file_router.py
+++ routers/fastapi_file_router.py
@@ -1,14 +1,14 @@
-from pathlib import Path
-from typing import List
+from fastapi import File, UploadFile, APIRouter
 
-from fastapi import FastAPI, File, UploadFile, APIRouter
-from schemas.responses import FileUploadResponse, FileDownloadResponse, FileReplaceResponse, FileDeleteResponse, \
-    FileRenameResponse, StandardResponse
-# from exceptions.custom_exceptions import
+from abstracts.exceptions_abstract import BaseCustomException
+from schemas.responses import FileDownloadResponse, StandardResponse, FileReplaceResponse, FileDeleteResponse, \
+    FileUploadResponse, FileRenameResponse
 from services.local_file_manager import LocalFileManager
 from services.aws_file_manager import AWSFileManager
 import os
 from io import BytesIO
+
+from utils.utils import create_file_metadata
 
 router = APIRouter()
 FILE_STORAGE_TYPE = os.getenv("FILE_STORAGE_TYPE", "local")
@@ -23,7 +23,7 @@ else:
 
 @router.get("/")
 async def files_root() -> StandardResponse:
-    """This is the root endpoint of the files API. It will return a simple message
+    """This is the root endpoint of the files section of the API. It will return a simple message
 
     Returns:
         A message welcoming the user to the files API
@@ -46,15 +46,17 @@ async def upload_file(file: UploadFile = File(...)) -> FileUploadResponse:
     """
     file_content = await file.read()
     file_id, message = file_manager.upload_file(BytesIO(file_content))
-    return FileUploadResponse(file_id=file_id, message=message, content_type=file.content_type, status_code=200)
+
+    return FileUploadResponse(file_id=file_id, message=message, content_type=file.content_type)
 
 
 @router.get("/download/{file_id}")
-async def download_file(file_id: str) -> FileDownloadResponse:
+async def download_file(file_id: str, download_name: str = None) -> FileDownloadResponse:
     """Download a file from the server.
 
     Args:
         file_id: Id of the file to download
+        download_name: Name to give the downloaded file
 
     Returns:
         File metadata and status.
@@ -63,5 +65,74 @@ async def download_file(file_id: str) -> FileDownloadResponse:
         FileDoesNotExistError: If the file does not exist
         FileDownloadError: If the file download fails
     """
-    message = file_manager.download_file(file_id)
-    return FileDownloadResponse(file_id=file_id, message=message, status_code=200)
+    try:
+        message = file_manager.download_file(file_id, download_name)
+    except BaseCustomException as exc:
+        exc.raise_as_http()
+    return FileDownloadResponse(file_id=file_id, message=message)
+
+
+@router.put("/replace/{file_id}")
+async def replace_file(file_id: str, file: UploadFile = File(...)) -> FileReplaceResponse:
+    """Replace a file on the server with a new file
+
+    Args:
+        file_id: Id of the file to replace
+        file: File to replace. Defaults to File(...) but will largely be images.
+
+    Returns:
+        File metadata and status.
+
+    Raises:
+        FileDoesNotExistError: If the file does not exist
+        FileUploadError: If the file upload fails
+    """
+    try:
+        file_content = await file.read()
+        message = file_manager.replace_file(file_id, BytesIO(file_content))
+    except BaseCustomException as exc:
+        exc.raise_as_http()
+    return FileReplaceResponse(file_id=file_id, message=message, content_type=file.content_type)
+
+
+@router.delete("/delete/{file_id}")
+async def delete_file(file_id: str) -> FileDeleteResponse:
+    """Delete a file from the server.
+
+    Args:
+        file_id: Id of the file to delete
+
+    Returns:
+        File metadata and status.
+
+    Raises:
+        FileDoesNotExistError: If the file does not exist
+        FileDeleteError: If the file delete fails
+    """
+    try:
+        message = file_manager.delete_file(file_id)
+    except BaseCustomException as exc:
+        exc.raise_as_http()
+    return FileDeleteResponse(file_id=file_id, message=message)
+
+
+@router.put("/rename/{file_id}")
+async def rename_file(file_id: str, new_file_id: str) -> FileRenameResponse:
+    """Rename a file on the server.
+
+    Args:
+        file_id: Id of the file to rename
+        new_file_id: New id of the file
+
+    Returns:
+        File metadata and status.
+
+    Raises:
+        FileDoesNotExistError: If the file does not exist
+        FileRenameError: If the file rename fails
+    """
+    try:
+        message = file_manager.rename_file(file_id, new_file_id)
+    except BaseCustomException as exc:
+        exc.raise_as_http()
+    return FileRenameResponse(old_file_id=file_id, new_file_id=new_file_id, message=message)
