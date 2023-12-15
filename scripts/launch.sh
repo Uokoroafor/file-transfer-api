b6c1f5bec0b4ee14176b5e0228b5bbeee8748ff4
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2023-12-15 22:42:25 +0000

    Pushing the scripts directory and bash scripts

---
+++ scripts/launch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# launch.sh is a script that launches the application
+uvicorn main:app --reload
---
+++ scripts/run_tests.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# run_tests.sh is a script that runs the tests
+
+# Run the tests
+pytest -v
+
+# Exit with the exit code of the tests
