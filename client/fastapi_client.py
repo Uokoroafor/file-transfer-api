a150153e65c143a688f8a57284d02978171f6e7a
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2023-12-15 22:42:52 +0000

    Pushing updated fastapi_client.py file

--- client/fastapi_client.py
+++ client/fastapi_client.py
@@ -1,5 +1,5 @@
+from typing import Optional, Dict
 import requests
-from typing import List, Optional, Dict
 
 
 class FastAPIClient:
@@ -11,7 +11,7 @@ class FastAPIClient:
         """
         self.base_url = base_url
 
-    def get(self, endpoint: str) -> requests.Response:
+    def _get(self, endpoint: str) -> requests.Response:
         """GET request to the API.
 
         Args:
@@ -22,7 +22,7 @@ class FastAPIClient:
         """
         return requests.get(self.base_url + endpoint)
 
-    def post(self, endpoint: str, body: Optional[Dict] = None) -> requests.Response:
+    def _post(self, endpoint: str, body: Optional[Dict] = None) -> requests.Response:
         """POST request to the API.
 
         Args:
@@ -34,7 +34,7 @@ class FastAPIClient:
         """
         return requests.post(self.base_url + endpoint, json=body)
 
-    def put(self, endpoint: str, body: Optional[Dict] = None) -> requests.Response:
+    def _put(self, endpoint: str, body: Optional[Dict] = None) -> requests.Response:
         """PUT request to the API.
 
         Args:
@@ -46,7 +46,7 @@ class FastAPIClient:
         """
         return requests.put(self.base_url + endpoint, json=body)
 
-    def delete(self, endpoint: str) -> requests.Response:
+    def _delete(self, endpoint: str) -> requests.Response:
         """DELETE request to the API.
 
         Args:
@@ -57,4 +57,91 @@ class FastAPIClient:
         """
         return requests.delete(self.base_url + endpoint)
 
-# TODO: Add the rest of the methods
+    def upload_file(self, file_path: str) -> requests.Response:
+        """Upload a file to the API.
+
+        Args:
+            file_path: Path to the file to upload.
+
+        Returns:
+            Response from the API.
+        """
+        with open(file_path, "rb") as f:
+            return self._post("/full/upload", body={"file": f})
+
+    def download_file(self, file_id: str) -> requests.Response:
+        """Download a file from the API.
+
+        Args:
+            file_id: ID of the file to download.
+
+        Returns:
+            Response from the API.
+        """
+        return self._get(f"/full/download/{file_id}")
+
+    def replace_file(self, file_id: str, file_path: str) -> requests.Response:
+        """Replace a file in the API.
+
+        Args:
+            file_id: ID of the file to replace.
+            file_path: Path to the file to replace with.
+
+        Returns:
+            Response from the API.
+        """
+        with open(file_path, "rb") as f:
+            return self._put(f"/full/replace/{file_id}", body={"file": f})
+
+    def rename_file(self, file_id: str, new_file_id: str) -> requests.Response:
+        """Rename a file in the API.
+
+        Args:
+            file_id: ID of the file to rename.
+            new_file_id: New ID of the file.
+
+        Returns:
+            Response from the API.
+        """
+        return self._put(f"/full/rename/{file_id}", body={"new_file_id": new_file_id})
+
+    def delete_file(self, file_id: str) -> requests.Response:
+        """Delete a file from the API.
+
+        Args:
+            file_id: ID of the file to delete.
+
+        Returns:
+            Response from the API.
+        """
+        self._delete(f"/full/delete/{file_id}")
+
+    def get_file_metadata(self, file_id: str) -> requests.Response:
+        """ Get file metadata from the API. This is a database operation.
+
+        Args:
+            file_id: ID of the file to get metadata for.
+
+        Returns:
+            Response from the API.
+        """
+        return self._get(f"/database/select/{file_id}")
+
+    def get_all_file_metadata(self) -> requests.Response:
+        """ Get all file metadata from the API. This is a database operation.
+
+        Returns:
+            Response from the API.
+        """
+        return self._get("/database/select_all")
+
+    def get_database_count(self) -> requests.Response:
+        """ Get the number of files in the database from the API. This is a database operation.
+
+        Returns:
+            Response from the API.
+        """
+        return self._get("/database/count")
+
+
+
