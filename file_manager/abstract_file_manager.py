af014297e85620be2b0f63155185191a1dfe7a76
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2023-12-22 12:25:24 +0000

    Updating file_manager

--- file_manager/abstract_file_manager.py
+++ file_manager/abstract_file_manager.py
@@ -1,23 +1,22 @@
 from abc import ABC, abstractmethod
-from typing import IO, Tuple
+from pathlib import Path
+from typing import Tuple, IO
 
 
 class AbstractFileManager(ABC):
 
     @abstractmethod
-    def upload_file(self, file:IO) -> Tuple[str, str]:
+    def upload_file(self, file: IO) -> Tuple[str, Path]:
         pass
 
     @abstractmethod
-    def download_file(self, file_id):
+    def download_file(self, file_id: str) -> Path:
         pass
 
     @abstractmethod
-    def rename_file(self, file_id, new_file_id):
+    def rename_file(self, file_id, new_file_id) -> Path:
         pass
 
     @abstractmethod
-    def delete_file(self, file_id):
+    def delete_file(self, file_id) -> str:
         pass
-
-
