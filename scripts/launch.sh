3fdb44f01aed7aaa9da0d6815e3a82cea95720a4
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-01-29 20:29:40 +0000

    Making scripts 'poetic':)

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 # This is a script that launches the app on a preconfigured port
+echo "Launching app via launch.sh..."
 
 # The default port to run the app on is 8000
 PORT=${1:-8000}
 
+# State what port the app is being launched from
+echo "Launching app on port $PORT..."
+
 # Run the app via uvicorn on the specified port
-uvicorn api.api:app --reload --port $PORT
+poetry run uvicorn api.api:app --reload --port $PORT &
+
+# Exit with the exit code of the uvicorn command
+trap "echo -e '\n App has been terminated...'; exit 0" SIGINT SIGTERM
+
+# Wait for the uvicorn command to finish
+wait $!
+
+# Exit with the exit code of the uvicorn command
+exit $?
--- scripts/setup_local_database.sh
+++ scripts/setup_local_database.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 # This is a script that sets up the local database by running the setup_local_database.py script in the app directory
 
+# State that the local database is being set up
+echo "Setting up local database..."
+
 # Run the setup_local_database.py script
-python database_manager/utils/setup_local_db.py
+poetry run python -m database_manager.utils.setup_local_db
+
+# Capture exit code
+EXIT_CODE=$?
+
+# If the exit code is 0, state that the local database has been set up
+if [ $EXIT_CODE -eq 0 ]; then
+    echo "Local database has been set up!"
+else
+    echo "Local database setup failed!"
+fi
 
 # Exit with the exit code of the setup_local_database.py script
-exit $?
+exit $EXIT_CODE
