a721ffd6e2f8bae74655f4be453947d28f3defc1
Ugo Okoroafor <39538470+Uokoroafor@users.noreply.github.com>
2024-02-05 18:25:58 +0000

    adding docstrings to filemanager

--- file_manager/abstract_file_manager.py
+++ file_manager/abstract_file_manager.py
@@ -1,22 +1,59 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
-from typing import Tuple, IO
+from typing import IO
 
 
 class AbstractFileManager(ABC):
+    """Abstract class for the file manager.
+
+    It contains methods that will be defined elsewhere in concrete classes
+    """
 
     @abstractmethod
     def upload_file(self, file: IO) -> Path:
+        """Abstract method to upload a file.
+
+        Args:
+            file: File to upload
+
+        Returns:
+            Path of the file uploaded.
+        """
         pass
 
     @abstractmethod
     def download_file(self, file_id: str) -> Path:
+        """Abstract method to download a file.
+
+        Args:
+            file_id: Id of the file to download
+
+        Returns:
+            Path of the file downloaded.
+        """
         pass
 
     @abstractmethod
     def rename_file(self, file_id, new_file_id):
+        """Abstract method to rename a file.
+
+        Args:
+            file_id: Id of the file to rename
+            new_file_id: New id of the file
+
+        Returns:
+            Path of the file renamed.
+        """
         pass
 
     @abstractmethod
     def delete_file(self, file_id):
+        """Abstract method to delete a file.
+
+        Args:
+            file_id: Id of the file to delete
+
+        Returns:
+            Path of the file deleted.
+        """
         pass
--- file_manager/local_file_manager.py
+++ file_manager/local_file_manager.py
@@ -10,13 +10,26 @@ from exceptions.file_exceptions import (FileDownloadError, FileUploadError, File
                                         FileUpdateError)
 
 load_dotenv()
-
 upload_path = Path(os.getenv("UPLOAD_DIRECTORY", default="data/uploads"))
 download_path = Path(os.getenv("DOWNLOAD_DIRECTORY", default="data/downloads"))
 
 
 class LocalFileManager(AbstractFileManager):
+    """Class for the local file manager."""
+
     def upload_file(self, file: IO) -> Path:
+        """Upload a file to the local file system.
+
+        Args:
+            file: File to upload.
+
+        Returns:
+            Path to the uploaded file.
+
+        Raises:
+            FileUploadError: If an error occurs while uploading the file.
+        """
+
         # generate a file id
         file_id = str(uuid4())
         file_location = upload_path / file_id
@@ -32,6 +45,19 @@ class LocalFileManager(AbstractFileManager):
         return file_location
 
     def download_file(self, file_id: str) -> Path:
+        """Download a file from the local file system.
+
+        Args:
+            file_id: ID of the file to download.
+
+        Returns:
+            Path to the downloaded file.
+
+        Raises:
+            FileDownloadError: If an error occurs while downloading the file.
+            FileDoesNotExistError: If the file does not exist.
+        """
+
         # Copy the file to the download path and return the path
         upload_file_path = upload_path / file_id
         download_file_path = download_path / file_id
@@ -45,6 +71,17 @@ class LocalFileManager(AbstractFileManager):
         return download_file_path
 
     def rename_file(self, file_id: str, new_file_id: str):
+        """Rename a file in the local file system.
+
+        Args:
+
+            file_id: ID of the file to rename.
+            new_file_id: New id of the file.
+
+        Raises:
+            FileDoesNotExistError: If the file does not exist.
+            FileUpdateError: If an error occurs while renaming the file.
+        """
         old_file_location = upload_path / file_id
         new_file_location = upload_path / new_file_id
 
@@ -56,6 +93,15 @@ class LocalFileManager(AbstractFileManager):
             raise FileUpdateError(f'Error occurred while renaming file: {e}')
 
     def delete_file(self, file_id: str):
+        """Delete a file from the local file system.
+
+        Args:
+            file_id: ID of the file to delete.
+
+        Raises:
+            FileDoesNotExistError: If the file does not exist.
+            FileDeleteError: If an error occurs while deleting the file.
+        """
         file_location = upload_path / file_id
         try:
             file_location.unlink()
